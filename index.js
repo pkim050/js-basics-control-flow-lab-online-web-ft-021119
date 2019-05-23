74732457624cdb262fd1ccaa562c267609ac2820
Patrick Kim <pkim050@gmail.com>
2019-05-23 23:00:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  if (number > 2500) {
+    return "No can do.";
+  }
+  else if (number > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (number <= 400) {
+    return "This one is on me!";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let temp = "";
+  city === "NYC" ? temp = "Ok, sounds good." : temp = "No go.";
+  return temp;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(string){
   // Write your code here!
+  switch (string) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye."
+  }
 }
