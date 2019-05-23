cd2a7a3586836f43bd907aa435cb28e64705f339
Patrick Kim <pkim050@gmail.com>
2019-05-23 22:52:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function scuberGreetingForFeet(number){
   // Write your code here!
-  if (number >= 400) {
+  if (number > 2500) {
+    return "No can do.";
+  }
+  else if (number > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (number <= 400) {
     return "This one is on me!";
   }
 }
